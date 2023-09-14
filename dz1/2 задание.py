46eb6bb69407323f3d867c43eab1b1b59bfebb28
AndryshaVoishev <143870928+AndryshaVoishev@users.noreply.github.com>
2023-09-14 15:44:10 +0300

    Add files via upload

---
+++ dz1/2 задание.py
@@ -0,0 +1,10 @@
+a = int(input(''))
+b = int(input(''))
+c = int(input(''))
+f = int(input(''))
+d = int(input(''))
+if f-d != 0:
+    ans = (a*b-c)/(f-d)
+    print(ans)
+else:
+    print('Делить на ноль нельзя')
---
+++ dz1/3 задание.py
@@ -0,0 +1,6 @@
+x = float(input('Введите число для x: '))
+y = float(input('Введите число для y: '))
+z = float(input('Введите число для z: '))
+
+ans = (x**2 + y**2 + z**2)**0.5
+print(ans)
