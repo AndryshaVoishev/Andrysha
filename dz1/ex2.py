edbd77d7839fe851da358b5c0c8dd641e56c8dc5
AndryshaVoishev <143870928+AndryshaVoishev@users.noreply.github.com>
2023-09-23 13:39:48 +0300

    domashka1

--- dz1/ex2.py
+++ dz1/ex2.py
@@ -1,10 +1,10 @@
-a = float(input(''))
-b = float(input(''))
-c = float(input(''))
-f = float(input(''))
-d = float(input(''))
-if f-d != 0:
-    ans = (a*b-c)/(f-d)
+a = float(input())
+b = float(input())
+c = float(input())
+f = float(input())
+d = float(input())
+if f - d != 0:
+    ans = (a * b - c) / (f - d)
     print(ans)
 else:
     print('Делить на ноль нельзя')
